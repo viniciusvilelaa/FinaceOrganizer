f9d5a8caa8d674ba6d88ed5818079d3366f3f4d1
viniciusvilelaa <ninobez@icloud.com>
2026-04-08 12:10:42 -0300

    Fix: Corrigindo rota da api

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { healthRouter } from "./health.routes.js";
 import { authRouter } from "./auth.routes.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { userRouter } from "../modules/users/user.routes.js";
+
 
 const router = Router();
 
 router.use("/health", healthRouter);
 router.use("/auth", authRouter);
+router.use("/users", userRouter);
 
 router.get("/me", authMiddleware, (req, res) => {
   return res.status(200).json({ user: req.user });
