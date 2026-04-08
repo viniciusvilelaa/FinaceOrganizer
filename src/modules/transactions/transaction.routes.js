a6c356495a578f93b98c0363b52ca979fb7c997f
viniciusvilelaa <ninobez@icloud.com>
2026-04-08 16:06:28 -0300

    Feat: implementado transaction.routes

--- src/modules/transactions/transaction.routes.js
+++ src/modules/transactions/transaction.routes.js
@@ -3,9 +3,11 @@ import express from "express"
 import { authMiddleware } from "../../middlewares/auth.middleware.js"
 import { asyncHandler } from "../../utils/async-handler.js"
 
+//Configurando config padrao para rotas
 const transactionRouter = express.Router();
 transactionRouter.use(authMiddleware);
 
+//Config dos endpoints
 transactionRouter.get("/", asyncHandler(transactionController.getAllTransactions));
 transactionRouter.get("/:id", asyncHandler(transactionController.getTransactionById));
 transactionRouter.post("/", asyncHandler(transactionController.createTransaction));
