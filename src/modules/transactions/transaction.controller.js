e0fe5b6611c555546ef90d22b9bc39d96144952b
viniciusvilelaa <ninobez@icloud.com>
2026-04-08 15:55:17 -0300

    Feat: Implementado funcoes no controller de transactions

---
+++ src/modules/transactions/transaction.controller.js
@@ -0,0 +1,34 @@
+import * as transactionService from "./transaction.service.js"
+
+//Endpoint para criaçao da transaction
+export const createTransaction = async (req, res) => {
+    const userId = req.user.sub;
+    const transaction = await transactionService.createTransaction(userId, req.body);
+    return res.status(200).json(transaction);
+}
+
+//Endpoint para getAll transactions
+export const getAllTransactions = async (req, res) => {
+  const userId = req.user.sub;
+  const transactions = await transactionService.getAllTransactions(userId);
+
+  return res.status(200).json(transactions);
+}
+
+//Endpoint para getById
+export const getTransactionById = async (req, res) => {
+    const userId = req.user.sub;
+    const transactionId = req.params.id;
+    const transaction = await transactionService.getTransactionById(userId, transactionId)
+
+    return res.status(200).json(transaction);
+}
+
+//Endpoint para DeleteById
+export const deleteTransaction = async (req, res) => {
+    const userId = req.user.sub;
+    const transactionId = req.params.id;
+    const transaction = await transactionService.deleteTransaction(userId, transactionId)
+
+    return res.status(200).json(transaction);
+}
