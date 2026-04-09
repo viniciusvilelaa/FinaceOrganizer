386c6ee16f855368f4a911ffcaa1451e6dd2a18e
viniciusvilelaa <ninobez@icloud.com>
2026-04-09 10:05:12 -0300

    Add: configurada rota de transaction no index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { healthRouter } from "./health.routes.js";
 import { authRouter } from "./auth.routes.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { userRouter } from "../modules/users/user.routes.js";
+import { transactionRouter } from "../modules/transactions/transaction.routes.js";
 
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.use("/health", healthRouter);
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
+router.use("/transactions", transactionRouter);
 
 router.get("/me", authMiddleware, (req, res) => {
   return res.status(200).json({ user: req.user });
