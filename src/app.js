b1672a3361aa0f71a7b724722da767e7a79a25c1
viniciusvilelaa <ninobez@icloud.com>
2026-05-11 16:12:41 -0300

    Feat: Implementado rate limiting para requisicoes HTTP no servidor

--- src/app.js
+++ src/app.js
@@ -6,8 +6,25 @@ import { env } from "./config/env.js";
 import { router } from "./routes/index.js";
 import { notFoundMiddleware } from "./middlewares/not-found.middleware.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {error: 'Too many HTTP requests, try again later'},
+  standardHeaders: true,
+  legacyHeaders: true
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10, // apenas 10 tentativas de login a cada 15 minutos
+  message: { error: 'Too many login attempts, please try again later.' }
+})
+
+app.use('/api/users/login', authLimiter)
+app.use(limiter);
 
 app.disable("x-powered-by");
 app.use(helmet());
