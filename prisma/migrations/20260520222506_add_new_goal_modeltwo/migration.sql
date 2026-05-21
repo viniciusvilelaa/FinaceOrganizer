7137dce28b7eb83a3fc14dcf87f70dc6c236f5f4
viniciusvilelaa <ninobez@icloud.com>
2026-05-21 09:52:25 -0300

    Feat(model): Adicionado novo elemento no banco de dados SQL, FinancialGoals para permitir definicoes de metas

---
+++ prisma/migrations/20260520222506_add_new_goal_modeltwo/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `FinancialGoal` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `targetAmount` DOUBLE NOT NULL,
+    `month` INTEGER NOT NULL,
+    `year` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `FinancialGoal` ADD CONSTRAINT `FinancialGoal_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
