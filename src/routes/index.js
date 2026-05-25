c4044b790daf53bb0ca0c06e7d5bce8ef82de999
viniciusvilelaa <ninobez@icloud.com>
2026-05-25 11:17:02 -0300

    Feat(financialGoal): Implementado router goals

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { authRouter } from "./auth.routes.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { userRouter } from "../modules/users/user.routes.js";
 import { transactionRouter } from "../modules/transactions/transaction.routes.js";
+import { financialGoalRouter } from "../modules/financialGoal/financialGoal.routes.js";
 
 
 const router = Router();
@@ -12,6 +13,7 @@ router.use("/health", healthRouter);
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 router.use("/transactions", transactionRouter);
+router.use("/goals", financialGoalRouter);
 
 router.get("/me", authMiddleware, (req, res) => {
   return res.status(200).json({ user: req.user });
