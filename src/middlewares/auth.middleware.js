2f1ff4b8c5874667e12e4fb27e418f9a44d0c02b
viniciusvilelaa <ninobez@icloud.com>
2026-05-27 11:35:38 -0300

    Fix(authMiddleware): Adicionado comentarios para ajudar no entendimento

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,13 +2,17 @@ import jwt from "jsonwebtoken";
 import { env } from "../config/env.js";
 import { ApiError } from "../utils/api-error.js";
 
+//Funcao de middleware para verificar se existe o token no cookies
 export function authMiddleware(req, res, next) {
+  //Obtem token pelo cookie
   const auth_token = req.cookies['auth_token'];
 
+  //Verifica se existe o token
   if(!auth_token){
     throw new ApiError(401, "User not authenticated");
   }
-
+  
+  //Try para decodificar o jwt e passar payload do user
   try{
     const decoded = jwt.verify(auth_token, process.env.JWT_SECRET);
 
