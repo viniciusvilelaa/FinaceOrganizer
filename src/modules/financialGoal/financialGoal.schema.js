24e8c203316b9c079061a3320805c5cef4564a4c
viniciusvilelaa <ninobez@icloud.com>
2026-06-17 08:35:36 -0300

    Feat(financialGoal): Implementado metodo getGoalHistory para retornar o historico de metas

--- src/modules/financialGoal/financialGoal.schema.js
+++ src/modules/financialGoal/financialGoal.schema.js
@@ -4,7 +4,7 @@ const currentDate = new Date().getFullYear();
 
 export const financialGoalSchema = z.object({
     name: z.string({ required_error: 'Name is required' }).max(15, "Name must be at most 15 characters"),
-    
+
     targetAmount: z.number({ required_error: 'Target amount is required' })
         .positive({ message: 'Target amount must be positive' }),
 
@@ -30,9 +30,9 @@ export const goalParamsSchema = z.object({
 });
 
 export const goalFiltersSchema = z.object({
-    status: z.enum(['ACHIEVED', 'FAILED']).optional(),
+    status: z.enum(['ACHIEVED', 'FAILED', "IN_PROGRESS"]).optional(),
     month: z.number().int("Month must be an integer").min(1, "Month must be at least 1").max(12, "Month must be at most 12").optional(),
     year: z.number().int("Year must be an integer").optional(),
     page: z.string().optional(),
-    limit: z.string.optional()
+    limit: z.string().optional()
 })
