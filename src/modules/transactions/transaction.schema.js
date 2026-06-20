2d5929cb10790476a79eb4dfc2b1a0f407211594
viniciusvilelaa <ninobez@icloud.com>
2026-06-20 11:17:46 -0300

    Feat(transactionSchema): Implementado schema zod para ser utilizado na criacao de transacoes

--- src/modules/transactions/transaction.schema.js
+++ src/modules/transactions/transaction.schema.js
@@ -1,5 +1,12 @@
 import { z } from 'zod';
 
+export const CreateTransactionSchema = z.object({
+    amount: z.coerce.int({required_error: 'Amount is required'}),
+    type: z.enum(['INCOME', 'EXPENSE'], {required_error: "Type is required"}),
+    category: z.enum(['COMIDA', 'TRANSPORTE', 'LAZER', 'SAUDE', 'EDUCACAO', 'MORADIA', 'OUTROS', 'INVESTIMENTO', 'SALARIO', 'ASSINATURAS', 'COMBUSTIVEL']),
+    description: z.string({required_error: 'Description is required'}).min(1)
+})
+
 export const transactionFiltersSchema = z.object({
     category: z.enum(['LAZER', 'COMIDA', 'TRANSPORTE']).optional(),
     description: z.string().min(1).optional(),
