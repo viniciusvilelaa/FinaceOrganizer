73902cee48021089b39633adfc4baa9ebabf9d1f
viniciusvilelaa <ninobez@icloud.com>
2026-06-27 11:07:50 -0300

    Refat(prisma.js): Removido adapter do maria db e alterado para postgress

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,22 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { env } from "../config/env.js";
 
 const globalForPrisma = globalThis;
 
-const adapter = new PrismaMariaDb({
-  host: new URL(env.databaseUrl).hostname,
-  port: Number(new URL(env.databaseUrl).port || 3306),
-  user: decodeURIComponent(new URL(env.databaseUrl).username),
-  password: decodeURIComponent(new URL(env.databaseUrl).password),
-  database: new URL(env.databaseUrl).pathname.replace("/", ""),
-  allowPublicKeyRetrieval: env.nodeEnv === "development",
-});
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
     log: env.nodeEnv === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
