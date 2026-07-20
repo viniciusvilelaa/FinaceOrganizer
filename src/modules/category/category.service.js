1472674635c3e273dea1b97d7df182827b876393
viniciusvilelaa <ninobez@icloud.com>
2026-07-20 16:16:59 -0300

    Feat(category.service): Implementada funcao de createCategory no service da entidade categoria

---
+++ src/modules/category/category.service.js
@@ -0,0 +1,40 @@
+import { prisma } from "../../lib/prisma.js";
+import { Prisma } from "@prisma/client";
+import { ApiError } from "../../utils/api-error.js";
+
+function sortCategories(categories) {
+  return categories.sort((a, b) => {
+    if (a.userId === null && b.userId !== null) return -1;
+    if (a.userId !== null && b.userId === null) return 1;
+    return a.name.localeCompare(b.name);
+  });
+}
+
+//Create new category
+export async function createCategory(userId, payload) {
+  const { name, color } = payload;
+
+  try {
+    const category = await prisma.category.create({
+      data: {
+        name,
+        color,
+        userId,
+      },
+    });
+
+    return category;
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      throw new ApiError(409, "You already have a category with this name.");
+    }
+
+    console.error("Error creating category:", error);
+    throw new ApiError(500, "Error when creating category.");
+  }
+}
+
+
