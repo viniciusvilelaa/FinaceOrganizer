24dea4537981f01410ac99345d0fc4c9ad2bb634
viniciusvilelaa <ninobez@icloud.com>
2026-07-21 17:09:01 -0300

    New(category.controller): Implementado endpoint de update

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -23,3 +23,27 @@ export const createCategory = async (req, res, next) => {
     next(err);
   }
 };
+
+//Endpoint updateCategory
+export const updateCategory = async (req, res, next) => {
+    
+    try{
+        const userId = req.user.sub;
+        const categoryId = Number(req.params.id);
+        const bodyParsed = updateCategorySchema.safeParse(req.body);
+
+        if (!bodyParsed.success) {
+            return res.status(400).json({
+            message: 'Invalid inputs for updating a category',
+            error: bodyParsed.error.flatten().fieldErrors,
+            });
+        }
+        
+        const updatedCategory = await categoryService.updateCategory(userId, categoryId, bodyParsed.data);
+
+        return res.status(200).json(updatedCategory)
+    } catch (err) {
+      next(err);
+    }
+
+}
