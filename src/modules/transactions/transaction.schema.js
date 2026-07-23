1aba152e8fccca337776299b9f24165f16cd7532
viniciusvilelaa <ninobez@icloud.com>
2026-07-23 09:39:06 -0300

    Fix(TransactionSchema): Ajustado schema zod do transaction para receber categoryId

--- src/modules/transactions/transaction.schema.js
+++ src/modules/transactions/transaction.schema.js
@@ -7,13 +7,13 @@ const categories = [
 export const transactionCreateSchema = z.object({
     amount: z.coerce.number({required_error: 'Amount is required'}),
     type: z.enum(['INCOME', 'EXPENSE'], {required_error: "Type is required"}),
-    category: z.coerce.number({required_error: 'Transaction Category is required'}),
+    categoryId: z.coerce.number({required_error: 'Transaction Category is required'}),
     description: z.string({required_error: 'Description is required'}).min(1),
     date: z.coerce.date({required_error: 'Date is required'})
 })
 
 export const transactionFiltersSchema = z.object({
-    category: z.enum(categories).optional(),
+    
     description: z.string().min(1).optional(),
     period: z.enum(['30d', '3m', '1y']).optional(),
     type: z.enum(['INCOME', 'EXPENSE']).optional(),
